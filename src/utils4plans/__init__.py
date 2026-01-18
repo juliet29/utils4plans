784d6099b6459d91fe8e1c6aaff72081693b7030
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2026-01-18 13:33:09 -0800

    add dynamic versioning to match git tags

--- src/utils4plans/__init__.py
+++ src/utils4plans/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from fputils!"
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.11a"  # Fallback for development mode
